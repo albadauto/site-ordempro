8c37fc457adf86d59eafc30ad47c60f3ce3e87b6
albadauto <joseadauto923@gmail.com>
2026-05-21 09:21:57 -0300

    Ultimas configs para prod

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/dSign' : '',
-  basePath: process.env.NODE_ENV === 'production' ? '/dSign' : ''
+
 }
 
 module.exports = nextConfig
